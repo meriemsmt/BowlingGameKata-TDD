a37af9e5193afe8e158c7fd4d2f19362a44f422c
meriemsmt <m.smati@esi-sba.dz>
2023-10-01 21:28:12 +0100

    Refactoring

--- test_bowling.py
+++ test_bowling.py
@@ -7,15 +7,17 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.g = Game()
 
+    def roll_many(self, n, pins):
+        for _ in range(n):
+            self.g.roll(pins)
+
     def test_zero_roll(self):
-        for i in range(20):
-            self.g.roll(0)
+        self.roll_many(20, 0)
         score = self.g.score()
         self.assertEqual(score,0)
 
     def test_all_ones(self):
-        for _ in range(20):
-            self.g.roll(1)
+        self.roll_many(20, 1)
         self.assertEqual(self.g.score(), 20)
 
 if __name__ == '__main__':
